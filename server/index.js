8b47b18b25763d8e0d93ecb720e62f6d9ac266dd
Nicolas Quiceno <nquicenob@gmail.com>
2019-07-12 02:09:23 +0200

    add cors server

--- server/index.js
+++ server/index.js
@@ -5,14 +5,18 @@ const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 8080;
 
 const server = cors_proxy.createServer({
-  removeHeaders: ['user-agent', 'host', 'origin', 'referer' ], // 'host',  'origin', 'referer'
+  removeHeaders: ['user-agent' ], // 'host',  'origin', 'referer'
   setHeaders: {
     'user-agent': 'Dummy Agent',
     'x-forwarded-for': '',
   },
-})
+});
+
+server.on('request', (req) => {
+  console.log(req.method);
+  console.log(req.headers);
+});
 
 server.listen(port, host, function() {
   console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
-
